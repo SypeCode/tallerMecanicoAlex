400ad3027ec4addb0b00546b22bc805a34358e22
SypeCode <sypecr@gmail.com>
2020-08-30 20:02:49 -0600

    Visualizacion de datos terminada: tabla y ticket

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,41 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {BrowserModule} from '@angular/platform-browser';
+import { Component, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
 /*import {cliente} from '../cliente';*/
 
 @Component({
-  selector: 'app-formulario',
-  templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  selector: "app-formulario",
+  templateUrl: "./formulario.component.html",
+  styleUrls: ["./formulario.component.css"],
 })
 export class FormularioComponent implements OnInit {
-
-  registro=[];
-  cliente:any;
-  nombre:string;
-  dui:string;
-  vehiculo:number;
-  costo:number;
-  detalle:string;
-  contador:number;
-  id:number;
-  constructor() { }
+  registro = [];
+  cliente: any;
+  nombre: string;
+  dui: string;
+  vehiculo: number;
+  costo: number;
+  detalle: string;
+  contador: number;
+  descuento: number;
+  visita: number;
+  total: number;
+  fecha: string;
+  constructor() {}
   ngOnInit() {
-  this.nombre="";
-  this.dui="";
-  this.vehiculo=0;
-  this.costo=0;
-  this.detalle="";
-  this.contador=0;
-  this.id=0;
+    this.nombre = "";
+    this.dui = "";
+    this.vehiculo = 0;
+    this.costo = 0;
+    this.detalle = "";
+    this.contador = 0;
+    this.descuento = 0;
+    this.visita = 0;
+    this.total = 0;
+    this.fecha = new Date().toLocaleDateString();
   }
- ingresar(){
+  ingresar() {
+    this.descuento = 0;
+    this.visita = 0;
 
-this.id =this.contador;
-this.cliente={"nombre":this.nombre,"dui":this.dui,"vehiculo":this.vehiculo,"costo":this.costo,"detalle":this.detalle, "id":this.id};
-this.registro.push(this.cliente);
-this.contador++;
+    for (let i = 0; i < this.registro.length; i++) {
+      if (this.registro[i].dui == this.dui) {
+        //Si existe la visita con el dui ingresado en todos los registros
+        this.visita++; //total de visitas para el dui ingresado
+      }
+    }
 
-}
+    if (this.visita == 1) {
+      this.descuento = this.costo * 0.05;
+      this.total = this.costo - this.descuento;
+    } else if (this.visita == 3) {
+      this.descuento = this.costo * 0.1;
+      this.total = this.costo - this.descuento;
+    } else {
+      this.total = this.costo;
+    }
 
+    this.cliente = {
+      nombre: this.nombre,
+      dui: this.dui,
+      vehiculo: this.vehiculo,
+      costo: this.costo,
+      descuento: this.descuento,
+      total: this.total,
+      detalle: this.detalle,
+      fecha: this.fecha
+    };
+    this.registro.push(this.cliente);
+    this.contador++;
+  }
 }
